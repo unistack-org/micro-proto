77c7563bd01ec756d900987b0ae1e9f8b5f8502c
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-02-28 00:37:11 +0300

    add micro specific proto stuff
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- generate.go
+++ generate.go
@@ -2,6 +2,7 @@ package main
 
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/annotations.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/http.proto
+//go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/micro.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/field_behavior.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/httpbody.proto
 //go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/annotations.proto
