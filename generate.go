acc73e64d8886471646dd920dc10bff223c146ca
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-08-17 00:55:55 +0300

    regen with new openapiv2
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- generate.go
+++ generate.go
@@ -6,5 +6,5 @@ package main
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/httpbody.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/micro.proto
 //go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/annotations.proto
-//go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/openapiv2.proto
+//go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/gopenapiv2.proto
 //go:generate protoc -I. -I./tag --go_out=paths=source_relative:. ./tag/tag.proto
