5355995d77c91e11f893654aed979593b3973090
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-12-30 14:20:18 +0300

    update
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- generate.go
+++ generate.go
@@ -4,7 +4,11 @@ package main
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/http.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/field_behavior.proto
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/httpbody.proto
+<<<<<<< HEAD
 //go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/micro.proto
+=======
+//go:generate protoc -I. -I./api --go_out=paths=source_relative:. ./api/client.proto
+>>>>>>> 75c244f1a60f (update)
 //go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/annotations.proto
 //go:generate protoc -I. -I./openapiv2 --go_out=paths=source_relative:. ./openapiv2/openapiv2.proto
 //go:generate protoc -I. -I./openapiv3 --go_out=paths=source_relative:. ./openapiv3/annotations.proto
